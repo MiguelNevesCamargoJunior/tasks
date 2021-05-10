35aa3779a56183f38aacc5e96838c1af4817aef0
MiguelNevesCamargoJunior <miguelnevescamargojunior@gmail.com>
2021-05-09 21:47:18 -0300

    desafio finalizado

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -1,5 +1,5 @@
 import { useState } from 'react'
-
+ 
 import '../styles/tasklist.scss'
 
 import { FiTrash, FiCheckSquare } from 'react-icons/fi'
@@ -16,14 +16,34 @@ export function TaskList() {
 
   function handleCreateNewTask() {
     // Crie uma nova task com um id random, não permita criar caso o título seja vazio.
+    if (!newTaskTitle) return;
+
+    const newTask = {
+      id: Math.random(),
+      title: newTaskTitle,
+      isComplete: false
+    }
+ 
+    setTasks(oldState => [ ...oldState, newTask ]);
+    setNewTaskTitle('');
   }
 
   function handleToggleTaskCompletion(id: number) {
     // Altere entre `true` ou `false` o campo `isComplete` de uma task com dado ID
+    const newTasks = tasks.map(task=>task.id === id ? {
+      ...task, isComplete: !task.isComplete
+    } : task);
+
+    setTasks(newTasks);
+
   }
 
   function handleRemoveTask(id: number) {
     // Remova uma task da listagem pelo ID
+
+    const filteredTasks = tasks.filter(task=> task.id != id);
+    setTasks(filteredTasks);
+
   }
 
   return (
@@ -49,7 +69,7 @@ export function TaskList() {
           {tasks.map(task => (
             <li key={task.id}>
               <div className={task.isComplete ? 'completed' : ''} data-testid="task" >
-                <label className="checkbox-container">
+                <label className="checkbox-container"> 
                   <input 
                     type="checkbox"
                     readOnly
